725c4161de24cb81bf1faefe37f7983d2f7c47db
Anoopcb <36423671+Anoopcb@users.noreply.github.com>
2020-04-27 15:33:04 +0100

    Update test_one_variables.py

---
+++ test_one_variables.py
@@ -0,0 +1,94 @@
+#print() function
+## first project, how to print in print function
+print("Hello world")
+print('Hello world')
+
+#string:-  collection of character inside "Double quotes"
+#or 'single quotes'
+# you can use 'single quotes' insides "double quotes" and
+#double quotes "" inside single quotes '' but you can not use
+#double quotes inside double quotes "" same with single quotes
+
+### please see below example
+print("Hello 'world' world")
+print('Hello "world" world')
+
+#Escape Sequences
+#\' single quote
+#\" double quote
+#\\ backslash
+#\n new line
+#\t tab
+#\b backspace
+
+print('Hello \'world\' world')
+print("Hello \"world\" world")
+print("Hello World\nHello World")
+
+#Comments, like this, python ignore all these hash comments
+#for out put Line A \n Line B
+print("Line A \\n Line B")
+print("#****print these following lines*****")
+print("This is \\\\ double blackslash")
+print("this is /\\/\\/\\/\\/\\ mountain")
+print("\\\" \\n \\t \\\' ")
+
+## if you put r before double quotes or single quotes in print
+## function then this will treat escape sequance as a normal test
+
+# how to print emoji
+print("\U0001F602")
+print("\U0001F604")
+
+#Python as a calculator
+
+## use print function and you can do any calculation inside it
+
+print(2+3)
+
+
+#Operators
+# + Additon
+# - Subtraction
+# * Multiplication
+# / Float division
+# // integer division
+# 5 Modulo, it gives remainder
+# ** Exponent
+
+## use bodmash rule  and
+##Precedence rule
+
+#####*** VARIABLES finally ******
+
+# varialbe ar just container which can store any data in memory
+## or you can say these are agents for a memory allotment
+## you can change value in variables
+### varibles are immutable
+
+number = 1
+print(number)
+number= "Anoop"
+print(number)
+
+#### variable rule
+
+##### you can't start a variable name by number
+#  2number = 44, will give a error
+
+####### second rule
+
+# can be start any letter or _
+
+print(number[0])### we will see these string topic
+
+
+#### convention
+
+## normally we use snake case writting
+
+### use_one_name
+
+##### useOneName--camel case writting, we should avoid this tye of writting
+
+
